386dd6f3fa19c44908e1bcf4c5c93558be7cda2f
Sehajbir Randhawa <srandhaw@purdue.edu>
2018-07-21 16:12:44 +0400

    created MeVC interface

---
+++ BreakPoint/BreakPoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  BreakPoint
+//
+//  Created by Sehajbir Randhawa on 7/21/18.
+//  Copyright © 2018 Sehajbir. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    //Outlets
+    @IBOutlet weak var profileImg: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+     
+    }
+    
+
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+    }
+    
+}
